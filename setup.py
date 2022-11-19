f85e2e677556909f3c73ca6e0b3ddc90faa2537b
Nigel Small <technige@nige.tech>
2022-11-19 13:13:32 +0000

    Fixed package description

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 
-# Get the long description from the README file
-long_description = (here / "README.md").read_text(encoding="utf-8")
-
 setup(
+    description="Python tools for the Fediverse.",
+    long_description=(here / "README.md").read_text(encoding="utf-8"),
+    long_description_content_type='text/markdown',
     packages=find_packages(exclude=["test", "test.*"]),
     python_requires=">=3.7, <4",
     install_requires=[
