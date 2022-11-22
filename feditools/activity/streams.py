917045f3253ab5465b7cb7af8ce9d8b920352e99
Nigel Small <technige@nige.tech>
2022-11-22 00:19:06 +0000

    Updated output format

--- feditools/activity/streams.py
+++ feditools/activity/streams.py
@@ -2,17 +2,21 @@
 
 
 class Link:
+    """
+    See: https://www.w3.org/TR/activitystreams-vocabulary/#dfn-link
+    """
 
     @classmethod
     def from_data(cls, data):
-        return cls(data["href"], href_lang=data.get("hreflang"), media_type=data.get("mediaType"),
-                   name=data.get("name"), id=data.get("id"), width=data.get("width"),
-                   height=data.get("height"), preview=data.get("preview"))
+        return cls(data["href"], href_lang=data.get("hreflang"), rel=data.get("rel"),
+                   media_type=data.get("mediaType"), name=data.get("name"), id=data.get("id"),
+                   width=data.get("width"), height=data.get("height"), preview=data.get("preview"))
 
-    def __init__(self, href, href_lang=None, media_type=None, name=None,
+    def __init__(self, href, href_lang=None, rel=None, media_type=None, name=None,
                  id=None, width=None, height=None, preview=None):
         self.href = href
         self.href_lang = href_lang
+        self.rel = rel
         self.media_type = media_type
         self.name = name
         self.id = id
@@ -24,6 +28,8 @@ class Link:
         args = [repr(self.href)]
         if self.href_lang:
             args.append(f"href_lang={self.href_lang!r}")
+        if self.rel:
+            args.append(f"rel={self.rel!r}")
         if self.media_type:
             args.append(f"media_type={self.media_type!r}")
         if self.name:
--- feditools/nodeinfo.py
+++ feditools/nodeinfo.py
@@ -1,35 +1,91 @@
 #!/usr/bin/env python
 
-
 from argparse import ArgumentParser
-from json import JSONDecodeError, dumps
+from json import JSONDecodeError
+from os import linesep
+from textwrap import wrap
 
 from urllib3 import PoolManager as HTTP
 
-from feditools.streams import Link
+from feditools.activity.streams import Link
 
 
-def get_node_info_links(host):
-    # returns the links to the node info itself
-    # https://github.com/jhass/nodeinfo/blob/main/PROTOCOL.md
-    http = HTTP()
-    rs = http.request("GET", f"https://{host}/.well-known/nodeinfo")
-    try:
-        data = rs.json()
-    except JSONDecodeError:
-        raise ValueError("Host does not export a well-known node info API")
-    else:
-        return [Link.from_data(link_data) for link_data in data["links"]]
+class NodeInfo:
+    """
+    See: https://github.com/jhass/nodeinfo/
+    """
 
-
-def get_node_info(host):
-    http = HTTP()
-    for link in get_node_info_links(host):
-        rs = http.request("GET", link.href)
+    @classmethod
+    def _get_links(cls, host):
+        # returns the links to the node info itself
+        # https://github.com/jhass/nodeinfo/blob/main/PROTOCOL.md
+        http = HTTP()
+        rs = http.request("GET", f"https://{host}/.well-known/nodeinfo")
         try:
-            yield rs.json()
+            data = rs.json()
         except JSONDecodeError:
-            raise ValueError("Broken link in node info: %r" % link)
+            raise ValueError("Host does not export a well-known node info API")
+        else:
+            return [Link.from_data(link_data) for link_data in data["links"]]
+
+    @classmethod
+    def get(cls, host):
+        http = HTTP()
+        for link in cls._get_links(host):
+            # TODO: check link.rel to know how to parse the response
+            rs = http.request("GET", link.href)
+            try:
+                info = rs.json()
+            except JSONDecodeError:
+                raise ValueError("Broken link in node info: %r" % link)
+            else:
+                yield cls(info["version"], info["software"], info["protocols"], info["services"],
+                          info["openRegistrations"], info["usage"], info["metadata"])
+
+    def __init__(self, version, software, protocols, services, open_registrations, usage, metadata):
+        self.version = version
+        """ The schema version, must be 1.0, 1.1, 2.0 or 2.1.
+        """
+        self.software = software
+        """ Metadata about server software in use.
+        """
+        self.protocols = protocols
+        """ The protocols supported on this server.
+        """
+        self.services = services
+        """ The third party sites this server can connect to via their application API.
+        """
+        self.open_registrations = open_registrations
+        """ Whether this server allows open self-registration.
+        """
+        self.usage = usage
+        """ Usage statistics for this server.
+        """
+        self.metadata = metadata
+        """ Free form key value pairs for software specific values. 
+            Clients should not rely on any specific key being present.
+        """
+
+    def __repr__(self):
+        return (f"{self.__class__.__name__}(version={self.version!r}, software={self.software!r}, "
+                f"protocols={self.protocols!r}, services={self.services!r}, "
+                f"open_registrations={self.open_registrations!r}, usage={self.usage!r}, metadata={self.metadata!r})")
+
+    def __str__(self):
+        lines = [
+            f"# NodeInfo version {self.version}",
+            f"software: {self.software['name']} {self.software['version']}",
+        ]
+        lines.extend(wrap("protocols: {}".format(", ".join(self.protocols)), subsequent_indent="  "))
+        lines.append("services:")
+        lines.extend(wrap("  inbound: {}".format(", ".join(self.services["inbound"])), subsequent_indent="    "))
+        lines.extend(wrap("  outbound: {}".format(", ".join(self.services["outbound"])), subsequent_indent="    "))
+        lines.append(f"open registrations: {self.open_registrations}")
+        lines.extend(wrap(f"usage: {self.usage}", subsequent_indent="  "))
+        lines.append("metadata:")
+        lines.extend(wrap("  {}".format(", ".join(f"{key}={value}" for key, value in self.metadata.items())),
+                          subsequent_indent="    ", break_on_hyphens=False))
+        return linesep.join(lines)
 
 
 def main():
@@ -37,9 +93,8 @@ def main():
     parser.add_argument("host")
     args = parser.parse_args()
     try:
-        for info in get_node_info(args.host):
-            # TODO: check rel to know how to parse the response
-            print(dumps(info))
+        for info in NodeInfo.get(args.host):
+            print(info)
     except ValueError as e:
         print(f"{e}")
         exit(1)
